983412e2fe8da1cd080f14cd9f8781ceb561cfb9
Shreya Kharche <kharcheshreyavijay@gmail.com>
2025-01-07 17:00:38 +0530

    new data

--- README.md
+++
@@ -1,2 +0,0 @@
-# Airtech-Ecosystem
-Airtech Ecosystem 
--- temp/script.js
+++ temp/script.js
@@ -3,25 +3,20 @@ const nav = document.querySelector(".nav"),
   navOpenBtn = document.querySelector(".navOpenBtn"),
   navCloseBtn = document.querySelector(".navCloseBtn");
 
-// Toggle search input and icon when search icon is clicked
 searchIcon.addEventListener("click", () => {
   nav.classList.toggle("openSearch");
   nav.classList.remove("openNav");
   if (nav.classList.contains("openSearch")) {
-    searchIcon.classList.replace("uil-search", "uil-times");
-  } else {
-    searchIcon.classList.replace("uil-times", "uil-search");
+    return searchIcon.classList.replace("uil-search", "uil-times");
   }
+  searchIcon.classList.replace("uil-times", "uil-search");
 });
 
-// Open navigation menu when menu button is clicked
 navOpenBtn.addEventListener("click", () => {
   nav.classList.add("openNav");
   nav.classList.remove("openSearch");
   searchIcon.classList.replace("uil-times", "uil-search");
 });
-
-// Close navigation menu when close button is clicked
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
